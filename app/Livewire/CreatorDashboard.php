a7e92e6d2416520043736724e50aae5865918231
farhanmuzakki213 <farhanmuzakki1p@gmail.com>
2026-02-13 21:10:50 +0700

    Setup-Project

---
+++ app/Livewire/CreatorDashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CreatorDashboard extends Component
+{
+    public function render()
+    {
+        return view('livewire.creator-dashboard');
+    }
+}
